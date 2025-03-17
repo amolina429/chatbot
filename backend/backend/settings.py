66e596b9cae4a55767058b5422682281b38fd12d
Andrés Molina <andresg.molina429@gmail.com>
2025-03-16 21:13:03 -0500

    fix: :bug: Solución de cors
    Se cambia el puerto 3000 de react, por el 5173 que usa vite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
